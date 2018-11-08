bb564fd68cec83a56ff794a21dcd5c6d3ed4b8d7
John Casabar <johncasabar@gmail.com>
2018-11-08 03:42:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,9 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade, id = nil)
-    
+    @name = name
+    @grade = grade
+    @id = id
   end
 
   def create_table
@@ -26,6 +28,15 @@ class Student
   end
 
   def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+    INSERT INTO students (name, grade) 
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def create(name, grade)
